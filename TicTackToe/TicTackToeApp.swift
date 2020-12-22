de658c537139bcadf864f393972d020ec297da9e
alex-babich <ababich.dev@gmail.com>
2020-12-22 10:53:21 +0200

    Initial Commit

---
+++ TicTackToe/TicTackToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTackToeApp.swift
+//  TicTackToe
+//
+//  Created by alex-babich on 22.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct TicTackToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
